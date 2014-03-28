693af56a46eb37463f6848af82df9fb4532847ed
prolin99 <prolin99@gmail.com>
2014-03-28 11:08:05 +0800

    加入匯入 EXCEL

--- admin/index.php
+++ admin/index.php
@@ -28,27 +28,51 @@ function f1(){
 		<form action ='{$_SERVER['PHP_SELF']}' enctype='multipart/form-data' method=post>
 		<fieldset>
 		<legend>台南市學生資料檔案匯入</legend>
-		<p><label>由學籍學生基本資料匯出至健康系統，取得全校資料。</label></p>
- 		<label>XML檔案</label>
-		<input type=file name=userdata><br/>
-		<input type='hidden' name='op' value='import'>
-		<button type='submit'  name='do_key' class='btn btn-primary'>同步</button>(  XML 要全校名單，舊學生名冊會先全部清除！)
-		</fieldset>
-		</form>" ;
+		<p><label>由學籍學生基本資料匯出至健康系統，取得全校資料。</label></p><br/>
+ 		<p><label>XML檔案 or EXCEL檔案:</label>
+		<input type=file name=userdata></p><br/><br/>
 		
+		<input type='hidden' name='op' value='import'>
+		<button type='submit'  name='do_key' class='btn btn-primary'>同步</button>(名冊資料庫會先全部清除！)<br/>
+		</fieldset>
+		</form>
+		<p>相關說明：
+		<a href='../health_sample.xml' target='_blank'>XML 範列檔</a>  ,<a href='../health_sample.xls' target='_blank'>EXCEL 範例檔</a><br/>
+		(只取欄位：身份證字號、姓名、性別、入學年、班級、監護人、座號、生日、代號)
+		</p>" ;
 		
  
   return $main;
 }
 
-//
+//匯入判別
 function import_stud(){
+	$file_up = XOOPS_ROOT_PATH."/uploads/" .$_FILES['userdata']['name'] ;
+	copy($_FILES['userdata']['tmp_name'] , $file_up );	
+	$main="開始匯入" . $file_up .'<br>';
+ 	 
+ 	$file_array= preg_split('/[.]/', $_FILES['userdata']['name'] ) ;
+ 	$ext= strtoupper(array_pop($file_array)) ;
+	if ($ext=='XML')  
+		import_xml($file_up) ;
+	if ($ext=='XLS') 
+		import_excel($file_up) ;	
+	if ($ext=='XLSX') 
+		import_excel($file_up , 2007) ;			
+	//刪除上傳的檔。
+	unlink($file_up)  ;
+ 
+	redirect_header($_SERVER['PHP_SELF'],3, '資料寫入!' );
+	
+	return $main; 
+ 
+}
+
+//xml 格式
+function import_xml($file_up){
 	global $xoopsDB;
 	
-	define("_FILES_XML",XOOPS_ROOT_PATH."/uploads/" ."stud.xml");
-	$main="開始匯入" . _FILES_XML .'<br>';
- 
-	copy($_FILES['userdata']['tmp_name'] , _FILES_XML );		
+
 	
 	//清空學資料庫中學生資料
 	$sql= "TRUNCATE TABLE   " . $xoopsDB->prefix("e_student")  ;
@@ -63,7 +87,7 @@ function import_stud(){
 	
    	//讀入 XML 檔案
  	$xmlDoc = new DOMDocument();
- 	$xmlDoc->load( _FILES_XML );
+ 	$xmlDoc->load( $file_up );
 	$x = $xmlDoc->documentElement;
  
 
@@ -104,14 +128,68 @@ function import_stud(){
 		
 	} 	
 	
-	//刪除上傳的 XML 檔。
-	unlink(_FILES_XML)  ;
- 
-	redirect_header($_SERVER['PHP_SELF'],3, '資料寫入!' );
-	
-	return $main;
+
 }
 
+//excel 格式
+function import_excel($file_up,$ver=5) {
+	global $xoopsDB;
+
+	//清空學資料庫中學生資料
+	$sql= "TRUNCATE TABLE   " . $xoopsDB->prefix("e_student")  ;
+	$result = $xoopsDB->queryF($sql) or redirect_header($_SERVER['PHP_SELF'],3, mysql_error());	
+	
+	//取得現在學年
+	$c_year = date("Y") -1911 ;
+	if  (date("m")<8) $c_year-=1 ;		
+	
+	include_once '../../tadtools/PHPExcel/IOFactory.php';
+	if ($ver ==5)
+		$reader = PHPExcel_IOFactory::createReader('Excel5');
+	else 	
+		$reader = PHPExcel_IOFactory::createReader('Excel2007');
+	
+	$PHPExcel = $reader->load( $file_up ); // 檔案名稱
+	$sheet = $PHPExcel->getSheet(0); // 讀取第一個工作表(編號從 0 開始)
+	$highestRow = $sheet->getHighestRow(); // 取得總列數
+ 
+	// 一次讀取一列
+	for ($row = 2; $row <= $highestRow; $row++) {
+		$v="";
+		//讀取一列中的每一格
+		for ($col = 0; $col <= 15; $col++) {
+			if ($col==6)
+				//生日
+				$val = PHPExcel_Shared_Date::ExcelToPHPObject( $sheet->getCellByColumnAndRow( $col , $row )->getValue())->format('Y-m-d');
+			else 
+				$val =  $sheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
+		 /*
+			//格式檢查(這部份有問題
+			if( PHPExcel_Shared_Date::isDateTime( $sheet->getCellByColumnAndRow($col , $row ) )){
+				$val = PHPExcel_Shared_Date::ExcelToPHPObject( $sheet->getCellByColumnAndRow( $col , $row )->getValue())->format('Y-m-d');
+			}else{
+				//$val =  $sheet->getCellByColumnAndRow($col, $row)->getCalculatedValue();
+				$val =  $sheet->getCellByColumnAndRow($col, $row)->getValue() ;
+			}
+ 		*/
+			if(!get_magic_quotes_runtime()) {
+				$v[$col]=addSlashes($val);
+			}else{
+				$v[$col]= $val ;
+			}
+			$stud_year = $c_year +1  -  $v[3] ; 	//入學年計算
+			$class_id  =  $stud_year*100 + $v[4] ;	//班級
+ 
+		}
+ 
+			$sql=  "INSERT INTO " . $xoopsDB->prefix("e_student") . 
+			           "  (`id`, `stud_id`, `name`, `person_id`, `birthday`, `class_id`, `class_sit_num`, `parent`, `chk_date`, `tn_id` ,sex ) 			        
+			            VALUES ('' , '{$v[15]}' , '{$v[1]}' , '{$v[0]}' , '{$v[6]}' , '$class_id' , '{$v[5]}' , '{$v[10]}' , now() , '{$v[15]}'  ,'{$v[2]}' ) " ;	   	
+ 
+			$result = $xoopsDB->query($sql) or redirect_header($_SERVER['PHP_SELF'],3, mysql_error());
+	}
+
+}
 
 /*-----------執行動作判斷區----------*/
 $op = empty($_REQUEST['op'])? "":$_REQUEST['op'];
--- xoops_version.php
+++ xoops_version.php
@@ -7,7 +7,7 @@
 
 //---基本設定---//
 $modversion['name'] = _MI_ESTUDENTS_NAME;			//模組名稱
-$modversion['version']	= '0.9';				//模組版次
+$modversion['version']	= '1.0';				//模組版次
 $modversion['author'] = _MI_ESTUDENTS_AUTHOR;			//模組作者
 $modversion['description'] = _MI_ESTUDENTS_DESC;		//模組說明
 $modversion['credits']	= _MI_ESTUDENTS_CREDITS;		//模組授權者
