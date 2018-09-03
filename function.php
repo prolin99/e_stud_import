930933b94da810a5312a4f57e92c3c3ba767180d
prolin99 <prolin99@gmail.com>
2018-09-03 08:53:20 +0800

    排序修正

--- function.php
+++ function.php
@@ -87,7 +87,7 @@ order by  u.user_occ ,c.class_id
             $xoopsDB->prefix('groups_users_link').'  AS g LEFT JOIN  '.$xoopsDB->prefix('users').'  AS u ON u.uid = g.uid '.
             ' left join '.$xoopsDB->prefix('e_classteacher').' as c on u.uid = c.uid '.
             "  WHERE g.groupid ='$teach_group_id'  group by u.uid  " ;
-    if (order_mode == 'staff')
+    if ($order_mode == 'staff')
       $sql .= " order by  c.staff , c.class_id , u.name ";
     else
       $sql .= " order by   u.uid DESC  ";
